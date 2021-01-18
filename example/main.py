ffdfb9dd75a0833b4b1038536efe57b3d0ea451a
tatarize <tatarize@gmail.com>
2021-01-18 11:11:40 -0800

    Plugin Example

--- example/main.py
+++ example/main.py
@@ -1,10 +1,8 @@
-from meerk40t.kernel import console_command, Modifier
+from meerk40t.kernel import console_command
 
 
-class Example(Modifier):
+def plugin(kernel):
+    @console_command(kernel, 'example', help="Says Hello World.")
+    def example_cmd(command, channel, _, args=tuple(), **kwargs):
+        channel(_('Hello World'))
 
-    @staticmethod
-    def sub_register(kernel):
-        @console_command(kernel, 'example', help="Says Hello World.")
-        def example_cmd(command, channel, _, args=tuple(), **kwargs):
-            channel(_('Hello World'))
