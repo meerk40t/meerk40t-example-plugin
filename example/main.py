a24fae6e7541f6616fdf91771ac2537a59fe4622
tatarize <tatarize@gmail.com>
2021-01-19 05:56:48 -0800

    Update API
    Changes to the API changes this command.

--- example/main.py
+++ example/main.py
@@ -1,8 +1,6 @@
-from meerk40t.kernel import console_command
-
 
 def plugin(kernel):
-    @console_command(kernel, 'example', help="Says Hello World.")
+    @kernel.console_command('example', help="Says Hello World.")
     def example_cmd(command, channel, _, args=tuple(), **kwargs):
         channel(_('Hello World'))
 
