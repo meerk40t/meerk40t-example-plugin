66d36c4d4eacbed7c96b7d9fc9a7c262d96f1d01
tatarize <tatarize@gmail.com>
2021-01-22 09:30:19 -0800

    Update API. Give lifecycle hooks.

--- example/main.py
+++ example/main.py
@@ -1,6 +1,14 @@
 
-def plugin(kernel):
-    @kernel.console_command('example', help="Says Hello World.")
-    def example_cmd(command, channel, _, args=tuple(), **kwargs):
-        channel(_('Hello World'))
+def plugin(kernel, lifecycle):
+    if lifecycle == 'register':
+        @kernel.console_command('example', help="Says Hello World.")
+        def example_cmd(command, channel, _, args=tuple(), **kwargs):
+            channel(_('Hello World'))
+    elif lifecycle == 'boot':
+        # Do some persistent actions or starts modules and modifiers
+        pass
+    elif lifecycle == 'shutdown':
+        # Meerk40t's closed adjust accordingly.
+        pass
+
 
