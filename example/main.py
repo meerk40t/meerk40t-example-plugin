edcac3baab7931db62704719c770f3632653a48d
Tatarize <tatarize@gmail.com>
2022-03-06 04:45:12 -0800

    Comment the different types of plugins lifecycles.

--- example/main.py
+++ example/main.py
@@ -1,6 +1,15 @@
 
 def module_plugin(module, lifecycle):
+    """
+    This plugin attaches to the module/wxMeerK40t for the opening and closing of the gui. If the gui is never launched
+    this plugin is never activated. wxMeerK40t is the gui wx.App object.
+
+    :param module:
+    :param lifecycle:
+    :return:
+    """
     if lifecycle == 'module':
+        # Responding to the "module" call makes this a module plugin for the specific module replied.
         return "module/wxMeerK40t"
     elif lifecycle == 'module_opened':
         pass
@@ -11,34 +20,65 @@ def module_plugin(module, lifecycle):
 
 
 def service_plugin(service, lifecycle):
+    """
+    This plugin attaches to the lhystudios devices. Any lhystudios device has each lifecycle event passed to this
+    plugin. There may be more than one such driver.
+
+    :param service:
+    :param lifecycle:
+    :return:
+    """
     if lifecycle == "service":
+        # Responding to the "service" call makes this a service plugin for the specific service replied.
         return "provider/device/lhystudios"
-    elif lifecycle == 'init':
-        pass
     elif lifecycle == 'added':
+        """
+        Service is added to the list of services for this provider type. In our example we are checking the device
+        service for the lhystudios driver.
+        """
         pass
     elif lifecycle == 'service_attach':
+        """
+        Our given service is attached. The current context.device is the 'service' passed in this plugin.
+        """
         pass
     elif lifecycle == 'assigned':
+        """
+        This is a plugin was started flagged to be assigned. For many drivers this launches their respective config
+        window.
+        """
         pass
     elif lifecycle == 'service_detach':
+        """
+        Our given service is no longer the context.device for the kernel.
+        """
         pass
     elif lifecycle == 'shutdown':
+        """
+        The service is shutdown.
+        """
         pass
 
 
 def plugin(kernel, lifecycle):
+    """
+    This is the kernel level plugin registration.
+
+    :param kernel:
+    :param lifecycle:
+    :return:
+    """
     if lifecycle == "plugins":
         return [service_plugin, module_plugin]
-    if lifecycle == 'init':
-        pass
     if lifecycle == 'preregister':
+        """
+        During the pre-register phase the module wxMeerK40t is registered and opened for the gui.
+        """
         pass
-
     if lifecycle == 'register':
         """
-        Register our changes to meerk40t. These should modify the registered values within meerk40t or install different
-        modules and modifiers to be used in meerk40t.
+        Register our various processes. These should modify the registered values within meerk40t. This stage is
+        used for general purpose value registrations.
         """
 
         # Example of writing an extension to plans. This can be added to the planner in console with "plan append hello"
@@ -60,44 +100,78 @@ def plugin(kernel, lifecycle):
             command will show up in the extended help for "help example".
             """
             channel(_('Hello World'))
+
     if lifecycle == 'configure':
+        """
+        Stage between registration and before the boot stages.
+        """
         pass
     elif lifecycle == 'preboot':
+        """
+        Preboot is usually where device services are started. Since many booting elements need to the devices to exist
+        services should be launched at this stage and prior to the boot.
+        """
         pass
     elif lifecycle == 'boot':
         """
-        Do some persistent actions or start modules and modifiers. Register any scheduled tasks or threads that need
-        to be running for our plugin to work. 
+        Start all services.
+        
+        Register any scheduled tasks or threads that need to be running for our plugin to work.
+        Register various choices within services which should all be started. 
         """
         pass
+    elif lifecycle == 'postboot':
+        """
+        Registers some additional choices such as some general preferences. 
+        """
     elif lifecycle == 'prestart':
+        """
+        CLI specified input file is loading during the pre-start phase
+        """
         pass
     elif lifecycle == 'start':
+        """
+        Nothing happens.
+        """
         pass
     elif lifecycle == 'poststart':
+        """
+        CLI specified output file is written during the poststart phase
+        """
         pass
     elif lifecycle == 'ready':
         """
-        Start process running. Sometimes not all modules and modifiers will be ready as they are processed in order
-        during boot. If your thread or work depends on other parts of the system being fully established they should 
-        work here.
+        Nothing happens.
         """
         pass
     elif lifecycle == 'finished':
+        """
+        Nothing happens.
+        """
         pass
     elif lifecycle == 'premain':
+        """
+        Nothing happens.
+        """
         pass
     elif lifecycle == 'mainloop':
         """
         This is the start of the gui and will capture the default thread as gui thread. If we are writing a new gui
-        system and we need this thread to do our work. It should be captured here. This is the main work of the program. 
+        system and we need this thread to do our work. It should be captured here. This is the main work of the program.
+        
+        You cannot ensure that more than one plugin can catch the mainloop. 
         """
         pass
     elif lifecycle == 'preshutdown':
+        """
+        Preshutdown saves the current activated device to the kernel.root to ensure it has the correct last value.
+        """
         pass
     elif lifecycle == 'shutdown':
         """
-        Meerk40t's closing down, our plugin should adjust accordingly. All registered meerk40t processes will be stopped
-        any plugin processes should also be stopped so the program can close correctly.
+        Meerk40t's closing down. Our plugin should adjust accordingly. All registered meerk40t processes will be stopped
+        any plugin processes should also be stopped so the program can close correctly. Depending on the order of
+        operations some operations might not be possible at this stage since the kernel will be in a partially shutdown
+        stage.
         """
         pass
